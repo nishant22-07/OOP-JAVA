2937fe009267e5b0df329cf458cbb8ec917d018d
nishant <nishantkudale04@gmail.com>
2025-01-05 22:56:24 +0530

    Add ObjectPrint class with custom toString implementation
    Introduced a new `ObjectPrint` class in the Polymorphism package. The class includes a custom `toString` method to format output and a `main` method to demonstrate object printing functionality.

---
+++ src/com/nishant/properties/Polymorphism/ObjectPrint.java
@@ -0,0 +1,20 @@
+package com.nishant.properties.Polymorphism;
+
+public class ObjectPrint {
+    int num;
+    public ObjectPrint(int num){
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectPrint{" +
+                "num=" + num +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        ObjectPrint obj = new ObjectPrint(57);
+        System.out.println(obj);
+    }
+}
