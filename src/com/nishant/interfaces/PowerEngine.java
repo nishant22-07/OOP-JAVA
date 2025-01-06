5d218effc300e0efa950fc157a5008a1272175ec
nishant <nishantkudale04@gmail.com>
2025-01-06 20:13:56 +0530

    Add interface-based car system with modular components
    Introduced interfaces and classes to create a modular car system. Includes `Engine`, `Media`, and `Brakes` interfaces, and implementations like `Car`, `Cdplayer`, `ElectricEngine`, and `NiceCar`. This design allows interchangeable components such as upgrading the engine or media system dynamically.

---
+++ src/com/nishant/interfaces/PowerEngine.java
@@ -0,0 +1,21 @@
+package com.nishant.interfaces;
+
+public class PowerEngine implements Engine {
+
+    @Override
+    public void start() {
+        System.out.println("Power engine starts");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Power engine stops");
+
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("Power engine accelerates");
+
+    }
+}
